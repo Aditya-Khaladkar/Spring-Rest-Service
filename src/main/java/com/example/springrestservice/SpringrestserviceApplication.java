3b102995579e0014421a3416ae5c1cc639090182
Aditya-Khaladkar <adityakhaladkar98@gmail.com>
2024-01-20 11:34:05 +0530

    first commit

---
+++ src/main/java/com/example/springrestservice/SpringrestserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrestserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springrestservice/SpringrestserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrestserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
